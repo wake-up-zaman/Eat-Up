8c16576cdb9dbe7033f6dee0f2c869d49c897eb3
wakeupzaman909 <hadiuzzaman909@outlook.com>
2023-03-17 21:11:10 +0600

    first commit

---
+++ eat-up-frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h2 className='text-rose-500'>Eat Up Test</h2>
+    </div>
+  );
+}
+
+export default App;
